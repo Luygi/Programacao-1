9ac17ce7e7070197d0e57ba037d891c2be67444d
DaywisonSilva <89873458dss>
2019-08-27 21:12:20 -0300

    atividades

--- src/com/unama/h.java
+++ src/com/unama/h.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class h {
 
     public static void main(String[] args){
-        float salario, reajuste;
+        float salario, reajuste, sreajuste;
         Scanner sc= new Scanner(System.in);
         System.out.println("Digite o valor do salário: R$");
         salario = sc.nextFloat();
         System.out.println("Digite o percentual de reajuste considerando 1 como 100%: ");
         reajuste = sc.nextFloat();
-        System.out.println("O valor do salário reajustado é de "+salario*reajuste+salario);
+        sreajuste = salario*reajuste+salario;
+        System.out.println("O valor do salário reajustado é de "+sreajuste);
     }
 }
