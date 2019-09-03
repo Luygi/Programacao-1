0262a8b639294377ad7d139ffb6d656462ee74f8
DaywisonSilva <54292958+DaywisonSilva@users.noreply.github.com>
2019-09-03 13:42:40 -0300

    Add files via upload

--- src/com/unama/i.java
+++ src/com/unama/i.java
@@ -1,8 +1,19 @@
-package com.unama;
+package app;
+
+import java.util.Scanner;
 
 public class i {
-
     public static void main(String[] args){
-       
+        float total;
+        float valor;
+        float desconto;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o valor da compra: R$");
+        valor = sc.nextFloat();
+        System.out.println("Digite o percentual de desconto, considerando 1 como 100%: ");
+        desconto = sc.nextFloat();
+        total = valor-(valor*desconto);
+        System.out.println("O valor da compra é igual a R$"+total); 
+        sc.close();
     }
 }
--- src/com/unama/j.java
+++ src/com/unama/j.java
@@ -1,8 +1,22 @@
-package com.unama;
+package app;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class j {
-
     public static void main(String[] args){
-       
+        Scanner sc = new Scanner(System.in);
+        float valor;
+        float dolar;
+        float conversao;
+        DecimalFormat formatador = new DecimalFormat("0.00");
+        System.out.print("Digite um valor em reais: R$");
+        valor = sc.nextFloat(); 
+        System.out.print("Digite a cotação do dólar: R$");
+        dolar = sc.nextFloat();
+        conversao = valor/dolar;
+        System.out.print("O valor convertido em dólar é igual a U$"+formatador.format(conversao));
+        sc.close();
+
     }
 }
