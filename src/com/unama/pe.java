f46651936fd380f59f4a28892dbc9968b23b1c1e
DaywisonSilva <54292958+DaywisonSilva@users.noreply.github.com>
2019-09-10 09:04:08 -0300

    Add files via upload

---
+++ src/com/unama/pe.java
@@ -0,0 +1,39 @@
+package com.unama;
+
+import java.util.Scanner;
+
+public class pe {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n1;
+        int n2;
+        int n3;
+        System.out.println("Digite o 1º número: ");
+        n1 = sc.nextInt();
+        System.out.println("Digite o 2º número: ");
+        n2 = sc.nextInt();
+        System.out.println("Digite o 3º número: ");
+        n3 = sc.nextInt();
+        if((n1<n2)&&(n2<n3)){
+            System.out.println(n1+","+n2+","+n3);
+        }
+        if((n1<n3)&&(n2>n3)){
+            System.out.println(n1+","+n3+","+n2);
+        }
+        if((n2<n1)&&(n3>n1)){
+            System.out.println(n2+","+n1+","+n3);
+        }
+        if((n2<n3)&&(n1>n3)){
+            System.out.println(n2+","+n3+","+n1);
+        }
+        if((n3<n2)&&(n1>n2)){
+            System.out.println(n1+","+n2+","+n3);
+        }
+        if((n3<n1)&&(n2>n1)){
+            System.out.println(n1+","+n2+","+n3);
+        }
+        
+
+
+    }
+}
